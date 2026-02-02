60aaf591cba1a3d2c4cf0a66ca391230ecd6d459
EmmaZechmann <emmazechmann@gmail.com>
2026-02-02 01:27:20 +0000

    done

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,16 +1,20 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Emma Zechmann
+#Date: 2/1/2026
+#Assignment: MAgic Eight ball 
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+    print("Magic 8 Ball")
   #Prompt the user for their question.
-
+    question = input("what is your question: ")
+    print(input)
+    Response = ["ew, no!" , "Absolutely" , "I guess so?", "Most likely", "Ask Again Later" , "Don't Count on It", "It is certain"]
+    Answer = random.choice(Response)
+    print(Answer)
   #Answer question randomly with one of the options from your earlier list.
 
 
